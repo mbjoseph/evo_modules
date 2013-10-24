c1b704f39acaeca189b057e2535781cb80b688ff
mbjoseph <maxwellbjoseph@gmail.com>
2013-10-23 18:52:56 -0600

    tweaked max values of slider bars for computational ease

--- fitness_landscape/ui.R
+++ fitness_landscape/ui.R
@@ -8,9 +8,9 @@ shinyUI(pageWithSidebar(
     sliderInput("strength", "Strength of selection", 
                 min=0.00001, max=10, value=1, step=0.0001),
     sliderInput("ngen", "Number of timesteps",
-                min=10, max=10000, value=1000, step=1), 
+                min=10, max=2000, value=1000, step=1), 
     sliderInput("mutation", "Mutation rate",
-                min=.01, max=10, value=.3, step=0.001)
+                min=.01, max=4, value=.3, step=0.001)
     ), 
   mainPanel(
     tabsetPanel(
